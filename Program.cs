90103eb50a0ae9bd7276813c959f1426ddaf6dcd
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-06-30 12:57:56 -0700

    create multiple inheritance using decorater design pattern

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace DecoratorMultipleInheritance
+{
+    public interface IBird
+    {
+        int Weight { get; set; }
+
+        void Fly();
+    }
+    public class Bird : IBird
+    {
+        public int Weight { get; set; }
+        public void Fly()
+        {
+            Console.WriteLine($"Soaring in the sky with weight {Weight}");
+        }
+    }
+
+    public interface ILizard
+    {
+        int Weight { get; set; }
+
+        void Crawl();
+    }
+    public class Lizard : ILizard
+    {
+        public int Weight { get; set; }
+        public void Crawl()
+        {
+            Console.WriteLine($"Crawling in the dirt with weight {Weight}");
+        }
+    }
+
+    public class Dragon : IBird, ILizard
+    {
+        private Bird bird = new Bird();
+        private Lizard lizard = new Lizard();
+        private int weight;
+
+        public int Weight { 
+            get { return weight; } 
+            set 
+            {
+                weight = value;
+                bird.Weight = value;
+                lizard.Weight = value;
+            }
+        }
+
+        public void Crawl()
+        {
+            lizard.Crawl();
+        }
+
+        public void Fly()
+        {
+            bird.Fly();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Dragon();
+            d.Weight = 100;
+            d.Fly();
+            d.Crawl();
+        }
+    }
+}
